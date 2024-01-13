c6ff964c85f61fde7c4a4e73e29f749926b603c9
kamchatka-volcano <gorelyy@eelnet.org>
2024-01-14 01:51:04 +0300

    -removed an implementation of StringConverter

--- include/figcone_tree/stringconverter.h
+++ include/figcone_tree/stringconverter.h
@@ -10,33 +10,7 @@
 namespace figcone {
 
 template<typename T>
-struct StringConverter {
-    static std::optional<T> fromString(const std::string& data)
-    {
-        [[maybe_unused]] auto setValue = [](auto& value, const std::string& data) -> std::optional<T>
-        {
-            auto stream = std::stringstream{data};
-            stream >> value;
-
-            if (stream.bad() || stream.fail() || !stream.eof())
-                return {};
-            return value;
-        };
-
-        if constexpr (std::is_convertible_v<std::string, tree::sfun::remove_optional_t<T>>) {
-            return data;
-        }
-        else if constexpr (tree::sfun::is_optional_v<T>) {
-            auto value = T{};
-            value.emplace();
-            return setValue(*value, data);
-        }
-        else {
-            auto value = T{};
-            return setValue(value, data);
-        }
-    }
-};
+struct StringConverter;
 
 } //namespace figcone
 
